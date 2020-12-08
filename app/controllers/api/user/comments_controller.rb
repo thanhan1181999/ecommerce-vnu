ddd9a2b0c071ec4ff5ab10d5f9a2f582695d6d3b
thanhan1181999 <nguyenthanhan1181999@gmail.com>
2020-12-08 21:00:46 +0700

    fix comment

--- app/controllers/api/user/comments_controller.rb
+++ app/controllers/api/user/comments_controller.rb
@@ -1,17 +1,25 @@
 class Api::User::CommentsController < ApplicationController
-  skip_before_action :authenticate_api_user!
   def create
-    order = Order.find_by(id: params[:order_id])
-    order.create_comment!(item_params)
-    json_response({alert: "comment success"})
+    order = Order.find_by(id: params[:order_id],user_id: current_api_user.id)
+    unless order.nil?
+      order.create_comment!(item_params)
+      json_response({alert: "comment success"})
+    else
+      json_response({alert: "not have order id = #{params[:order_id]}"})
+    end
   end
 
   def show
   end
 
   def destroy
-    Comment.find_by(id: params[:comment_id])&.destroy
-    json_response({alert: "delete comment success"})
+    comment = Comment.find_by(id: params[:comment_id])
+    if comment.order.user_id == current_api_user.id
+      comment.destroy
+      json_response({alert: "delete comment success"})
+    else
+      json_response({alert: "you dont have this comment"})
+    end
   end
 
   private
